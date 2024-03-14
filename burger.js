87a927a8419c100ce47f14ec4ee8265b09e0f979
Mishamaev <130150556+Mishamaev@users.noreply.github.com>
2024-03-14 18:41:51 +0200

    add files

---
+++ burger.js
@@ -0,0 +1,7 @@
+// console.log("amogus us")
+let burger = document.querySelector(".burger")
+let menu = document.querySelector(".menu")
+
+burger.addEventListener("click", function() {
+   menu.classList.toggle("open")
+})
